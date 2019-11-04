244c6ff016cd9bd9894441a0f79fe02161fa050f
jsjtxietian <jsjtxietian@outlook.com>
2019-11-04 12:53:09 +0800

    migrate ok

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import D3 from '../D3'
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+	
+	render() {
+		return <D3></D3>
+	}
+}
+
+
+export default App;
---
+++ src/D3/index.js
@@ -0,0 +1,3 @@
+import D3 from './D3.js'
+
+export default D3;
