1e3cbafff086c85f5e0885abc139d5c465c44170
jsjtxietian <jsjtxietian@outlook.com>
2019-11-05 22:11:40 +0800

    animation done

--- src/D3/index.js
+++ src/D3/index.js
@@ -5,9 +5,10 @@ import './D3.css'
 //todo
 //1. do not change state directly 
 //2. convert to hooks
+//3. change tooptip
 
 const Countries = ["Asia", "Europe", "Africa", "SouthAmerica", "Oceania", "NorthAmerica"];
- 
+const Duration = 500;
 
 class D3 extends Component {
 
@@ -20,6 +21,7 @@ class D3 extends Component {
 			xBias: 60,
 			xScale: null,
 			yScale: null,
+			isFirst: true
 		};
 
 		this.drawAll = this.drawAll.bind(this);
@@ -29,86 +31,146 @@ class D3 extends Component {
 		this.bindEvents = this.bindEvents.bind(this);
 	}
 
+	drawAll(d) {
+		this.createSVG();
+		this.drawAxes(this.props.x_attr, this.props.y_attr);
+		this.drawDataPoints(this.props.x_attr, this.props.y_attr);
+		this.beautify();
+		this.bindEvents();
+	};
+
+	//axis "x" / "y"
+	//type 0(0,max) 1(min,max)
+	scaleAxis(axis, type, x, y) {
+		let scale = null;
+
+		if (type === 0) {
+			let max = d3.extent(this.props.data, function (d) {
+				return parseFloat(d[axis === "x" ? x : y])
+			})[1];
+			scale = d3.scaleLinear()
+				.domain([0, max])
+				.range(axis === "x" ? [this.state.xBias, this.state.size[0]] : [this.state.size[1], 60]);
+		}
+		else {
+			scale = d3.scaleLinear()
+				.domain(d3.extent(this.props.data, function (d) {
+					return parseFloat(d[axis === "x" ? x : y])
+				}))
+				.range(axis === "x" ? [this.state.xBias, this.state.size[0]] : [this.state.size[1], 60]);
+		}
+
+		if (axis === "x") {
+			this.state.xScale = scale;
+		}
+		else {
+			this.state.yScale = scale;
+		}
+	}
 
 	drawAxes(x, y) {
 
 		let formatPercent = d3.format(".2%");
+		let xAxis = d3.axisBottom().scale(this.state.xScale).tickFormat(formatPercent);
+		let yAxis = d3.axisLeft().scale(this.state.yScale).tickFormat(formatPercent);
 
-		let xMax = d3.extent(this.props.data, function (d) {
-			return parseFloat(d[x])
-		})[1];
-		/**specify scale**/
-		this.state.xScale = d3.scaleLinear()
-			.domain([0, xMax])
-			.range([this.state.xBias, this.state.size[0]]);
+		if (x === "Birth Rate" || x === "Death Rate") {
+			this.scaleAxis("x", 0, x, y);
+			xAxis = d3.axisBottom().scale(this.state.xScale).tickFormat(formatPercent);
+		}
+		else {
+			this.scaleAxis("x", 1, x, y);
+			xAxis = d3.axisBottom().scale(this.state.xScale);
+		}
 
-		let yMax = d3.extent(this.props.data, function (d) {
-			return parseFloat(d[y])
-		})[1];
-		this.state.yScale = d3.scaleLinear()
-			.domain([0, yMax])
-			.range([this.state.size[1], 30]);
-
-		/**create axis object **/
-		var xAxis = d3.axisBottom().scale(this.state.xScale).tickFormat(formatPercent);
-		var yAxis = d3.axisLeft().scale(this.state.yScale).tickFormat(formatPercent);
+		if (y === "Birth Rate" || y === "Death Rate") {
+			this.scaleAxis("y", 0, x, y);
+			yAxis = d3.axisLeft().scale(this.state.yScale).tickFormat(formatPercent);
+		}
+		else {
+			this.scaleAxis("y", 1, x, y);
+			yAxis = d3.axisLeft().scale(this.state.yScale);
+		}
 
 		/**create container for axis */
-		let x_container = d3.select("#main_svg")
-			.append("g")
-			.attr("id", "xaxis")
-			.attr("class", "axis")
-			.attr("transform", "translate(0," + this.state.size[1] + ")");
 
-		let y_container = d3.select("#main_svg")
-			.append("g")
-			.attr("id", "yaxis")
-			.attr("class", "axis")
-			.attr("transform", "translate(60,0)");
+		if (this.state.isFirst) {
+			let x_container = d3.select("#main_svg")
+				.append("g")
+				.attr("id", "xaxis")
+				.attr("class", "axis")
+				.attr("transform", "translate(0," + this.state.size[1] + ")");
 
-		/**Apply axis object to container */
-		x_container.call(xAxis)
-		y_container.call(yAxis)
+			let y_container = d3.select("#main_svg")
+				.append("g")
+				.attr("id", "yaxis")
+				.attr("class", "axis")
+				.attr("transform", "translate(60,0)");
+			x_container.call(xAxis);
+			y_container.call(yAxis);
+			/**Add axis label */
+			x_container.append("text")
+				.text(x)
+				.attr("class", "axistext")
+				.attr("id", "xAxisText")
+				.attr("x", 580)
+				.attr("y", -10);
 
-		/**Add axis label */
-		x_container.append("text")
-			.text(x)
-			.attr("class", "axistext")
-			.attr("x", 580)
-			.attr("y", -10);
+			y_container.append("text")
+				.text(y)
+				.attr("transform", "rotate(-90)")
+				.attr("class", "axistext")
+				.attr("id", "yAxisText")
+				.attr("x", -28)
+				.attr("y", 18);
+		}
+		else {
+			d3.selectAll("#xaxis").transition().duration(Duration).call(xAxis);
+			d3.selectAll("#yaxis").transition().duration(Duration).call(yAxis);
+			d3.select("#xAxisText").transition().duration(Duration).text(x);
+			d3.select("#yAxisText").transition().duration(Duration).text(y);
+		}
 
-		y_container.append("text")
-			.text(y)
-			.attr("transform", "rotate(-90)")
-			.attr("class", "axistext")
-			.attr("x", -28)
-			.attr("y", 18);
 	}
 
 	createSVG() {
-		d3.select(this.refs.main_div)
-			.append("svg")
-			.attr("width", "100%")
-			.attr("height", "100%")
-			.attr("id", "main_svg");
+		if (this.state.isFirst) {
+			d3.select(this.refs.main_div)
+				.append("svg")
+				.attr("width", "100%")
+				.attr("height", "100%")
+				.attr("id", "main_svg");
+		}
 	};
 
 	drawDataPoints(x, y) {
-		/**create container for points */
-		let plot = d3.select("#main_svg")
-			.append("g")
-			.attr("id", "plots");
 
-		/**initiate data binding */
-		let plotEnter = plot.selectAll("circle")
+		if (this.state.isFirst) {
+			/**create container for points */
+			let plot = d3.select("#main_svg")
+				.append("g")
+				.attr("id", "plots");
+
+			/**initiate data binding */
+			let plotEnter = plot.selectAll("circle")
+				.data(this.props.data, function (d) { return d["Country"] })
+				.enter();
+
+			/**Append circle according to data */
+			plotEnter.append("circle")
+				.attr("cx", (d) => { return this.state.xScale(d[x]) })
+				.attr("cy", (d) => { return this.state.yScale(d[y]) })
+				.attr("r", 5);
+		}
+		else{
+			d3.selectAll("circle")
 			.data(this.props.data, function (d) { return d["Country"] })
-			.enter();
-
-		/**Append circle according to data */
-		plotEnter.append("circle")
-			.attr("cx", (d) => { return this.state.xScale(d[x]) })
-			.attr("cy", (d) => { return this.state.yScale(d[y]) })
-			.attr("r", 5);
+			.transition()
+			.duration(Duration)
+			.delay(Duration)
+			.attr("cx",(d) => this.state.xScale(d[x]))
+			.attr("cy",(d) => this.state.yScale(d[y]));
+		}
 	}
 
 	getRegionIndex = (strRegion) => {
@@ -121,70 +183,70 @@ class D3 extends Component {
 	};
 
 	beautify() {
-		/**distinguish data points by color */
-		d3.selectAll("circle")
-			.style("fill", (d) => { return this.state.color(this.getRegionIndex(d["Region"])) });
 
-		for (let i = 0; i < Countries.length; i++) {
-			d3.select("#main_svg")
-				.append("text")
-				.attr("x", 655)
-				.attr("y", 32 + i * 20)
-				.text(Countries[i]);
+		if (this.state.isFirst) {
+			/**distinguish data points by color */
+			d3.selectAll("circle")
+				.style("fill", (d) => { return this.state.color(this.getRegionIndex(d["Region"])) });
 
-			d3.select("#main_svg")
-				.append("rect")
-				.attr("x", 630)
-				.attr("y", 18 + i * 20)
-				.attr("width", 20)
-				.attr("height", 15)
-				.style("fill", this.state.color(i))
-				.attr("id", i)
-				.attr("class", "legend");
+			for (let i = 0; i < Countries.length; i++) {
+				d3.select("#main_svg")
+					.append("text")
+					.attr("x", 655)
+					.attr("y", 32 + i * 20)
+					.text(Countries[i]);
+
+				d3.select("#main_svg")
+					.append("rect")
+					.attr("x", 630)
+					.attr("y", 18 + i * 20)
+					.attr("width", 20)
+					.attr("height", 15)
+					.style("fill", this.state.color(i))
+					.attr("id", i)
+					.attr("class", "legend");
+			}
 		}
 
 	}
 
 	bindEvents() {
-		/*Display Name on Hover*/
-		d3.select("#plots")
-			.append("text")
-			.attr("id", "country_name");
+		if (this.state.isFirst) {
+			/*Display Name on Hover*/
+			d3.select("#plots")
+				.append("text")
+				.attr("id", "country_name");
 
-		d3.selectAll("circle")
-			.on("mouseover", (d) => {
-				/*move and set name tag */
-				d3.select("#country_name")
-					.text(d['Country'])
-					.attr("x", this.state.xScale(d[this.props.x_attr]))
-					.attr("y", this.state.yScale(d[this.props.y_attr]) - 10);
-			})
-			.on("mouseout", function (d) {
-				d3.select("#country_name").text("");
-			})
-			.on("click", function (d) {
-				
-				d3.select("#country_name").text("");
-				d3.select(this).remove();
-			})
+			d3.selectAll("circle")
+				.on("mouseover", (d) => {
+					/*move and set name tag */
+					d3.select("#country_name")
+						.text(d['Country'])
+						.attr("x", this.state.xScale(d[this.props.x_attr]))
+						.attr("y", this.state.yScale(d[this.props.y_attr]) - 10);
+				})
+				.on("mouseout", function (d) {
+					d3.select("#country_name").text("");
+				})
+				.on("click", function (d) {
+
+					d3.select("#country_name").text("");
+					d3.select(this).remove();
+				})
+
+		}
 	}
 
-	drawAll(d) {
-		this.createSVG();
-		this.drawAxes(this.props.x_attr, this.props.y_attr);
-		this.drawDataPoints(this.props.x_attr, this.props.y_attr);
-		this.beautify();
-		this.bindEvents();
-	};
 
 	componentDidMount() {
 		this.drawAll(this.props.data);
+		this.state.isFirst = false;
 	}
 
 	componentDidUpdate(prevProps, prevState) {
 		this.drawAll(this.props.data);
 	}
-	
+
 
 	render() {
 		return <div className="main_div" ref="main_div" ></div>
